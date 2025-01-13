982b3fc68bb3afbd13c456007b1d83272a8db3e9
Didem Benlioglu <didem.benlioglu@gmail.com>
2025-01-13 12:15:07 +0100

    fixed test code syntax

--- test_app.py
+++ test_app.py
@@ -1,15 +1,15 @@
 import app
 
 def test_home():
-    #test client olustur
+    # test client oluştur
     client = app.app.test_client()
     response = client.get("/")
     assert response.status_code == 200
     assert b"Hos Geldiniz!" in response.data
 
-def test.about():
-    #about endpointinini test et
+def test_about():
+    # about endpointini test et
     client = app.app.test_client()
     response = client.get("/about")
     assert response.status_code == 200
-    assert b"Bu basit bir flask uygulamasidir."
+    assert b"Bu basit bir flask uygulamasidir." in response.data
